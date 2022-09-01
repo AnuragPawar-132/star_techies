9ee7f68e7311f485fe37fb0a326fcb5e905c6c2c
AnuragPawar-132 <anuragpawar.me@gmail.com>
2022-09-01 17:02:06 +0530

    files structure is created

---
+++ star-techies/src/components/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import Crypto from '../pages/Crypto';
+import ShareMarket from '../pages/ShareMarket';
+
+const AllRoutes = () => {
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<Crypto/>} />
+            <Route path="/sharemarket" element={<ShareMarket/>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default AllRoutes
