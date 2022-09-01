3b91c90d3db6c9388c053fa3874906b754ba60e1
AnuragPawar-132 <anuragpawar.me@gmail.com>
2022-09-01 16:30:22 +0530

    initial commit

---
+++ star-techies/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Goa Singham</h1>
+      <input placeholder="Goa" />
+    </div>
+  );
+}
+
+export default App;
